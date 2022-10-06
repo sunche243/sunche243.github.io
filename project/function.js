d67f139c91703ea352be0cec935bc08aa7be3000
sunche243 <starcj7@naver.com>
2022-10-06 13:11:18 +0900

    edit codes & add font

--- project/function.js
+++ project/function.js
@@ -1,16 +1,37 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+// // function myFunction() {
+// //     document.getElementById("myDropdown").classList.toggle("show");
+// //   }
   
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+// //   window.onclick = function(event) {
+// //     console.log('버튼이 눌렸습니다');
+// //     if (!event.target.matches('.dropbtn')) {
+// //       var dropdowns = document.getElementsByClassName("dropdown-content");
+// //       var i;
+// //       for (i = 0; i < dropdowns.length; i++) {
+// //         var openDropdown = dropdowns[i];
+// //         if (openDropdown.classList.contains('show')) {
+// //           openDropdown.classList.remove('show');
+// //         }
+// //       }
+// //     }
+// //   }
+
+
+
+// function myFunction() {
+//     document.getElementById("myDropdown").className +='show'
+//   }
+  
+//   window.onclick = function(event) {
+//     console.log('버튼이 눌렸습니다');
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
