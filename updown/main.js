9742f8dc52b26ca00a0bb1695494b9f04c80c1e0
sunche <88880872+sunche243@users.noreply.github.com>
2023-03-10 10:49:17 +0900

    Add function

--- updown/main.js
+++ updown/main.js
@@ -7,70 +7,71 @@ if(guess == 50 || guess == 0){
     var guess = Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+console.log(guess)
+
 document.addEventListener("keyup", function(event) {
     if(event.code === 'Enter') {
         bring();
     };
 });
 
+function add_attempt(){
+    
+    attempt++; 
+    if(attempt == 5){
+        document.getElementById('attem').innerHTML = '마지막 시도';
+    }else if(attempt == 6){
+        alert('실패') ;
+                    
+    }else{
+        document.getElementById('attemptn').innerHTML = attempt;
+    };
+
+};
+
 function bring(){
 
-        var input = document.getElementById('ans').value;
-        input = parseInt(input)
+    var input = document.getElementById('ans').value;
+    input = parseInt(input)
 
-        if(min < input && input < max)
+    if(min < input && input < max)
+    {
+        if(input > guess)
         {
-            if(input > guess)
-            {
-                document.getElementById('maxn').innerHTML = input;
-                max = input;
+            document.getElementById('maxn').innerHTML = input;
+            max = input;
 
-                attempt++; 
-                if(attempt == 5){
-                    document.getElementById('attem').innerHTML = '마지막 시도';
-                }else if(attempt == 6){
-                    alert('실패') ;
-                    location.reload();
-                }else{
-                    document.getElementById('attemptn').innerHTML = attempt;
-                };
-            }
-            else if(input < guess)
-            {
-                document.getElementById('minn').innerHTML = input;
-                min = input;
+            add_attempt();
 
-                attempt++; 
-                if(attempt == 5){
-                    document.getElementById('attem').innerHTML = '마지막 시도';
-                }else if(attempt == 6){
-                    alert('실패') ;
-                    location.reload();
-                }else{
-                    document.getElementById('attemptn').innerHTML = attempt;
-                };
-            }
-            else
-            {
-                alert('정답');
-                location.reload();
-            };
+        }
+        else if(input < guess)
+        {
+            document.getElementById('minn').innerHTML = input;
+            min = input;
+
+            add_attempt();
         }
         else
         {
-            if(!input)
-            {
-                alert('값을 입력하세요');
-            }
-            else if(input >= max)
-            {
-                alert(max + "보다 작은 값을 입력하세요");
-            }
-            else if(input <= min)
-            {
-                alert(min + "보다 큰 값을 입력하세요");
-            };
+            alert('정답');
+            location.reload();
         };
+    }
+    else
+    {
+        if(!input)
+        {
+            alert('값을 입력하세요');
+        }
+        else if(input >= max)
+        {
+            alert(max + "보다 작은 값을 입력하세요");
+        }
+        else if(input <= min)
+        {
+            alert(min + "보다 큰 값을 입력하세요");
+        };
+    };
 
     document.getElementById('ans').value = null;
 
